3f2010c9620180ef6ecb1bf949665f8c9bf5d7f3
Kathy Kim <kitkathy5192@gmail.com>
2022-02-07 13:01:22 -0500

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Kathy Kim on 2/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
